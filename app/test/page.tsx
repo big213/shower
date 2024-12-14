1937382c8e850939de5cac00459bf1d34a876468
big213 <jc4095@nyu.edu>
2024-12-14 12:58:04 -0500

    test commit

---
+++ app/test/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Hello, World!</h1>
+      <p>Welcome to my Next.js app!</p>
+    </div>
+  );
+}
